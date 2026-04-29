3c1b63e3286ea562b5eab2dc50e3d82f23886c9d
gabrielqueiroz1 <gd37420@hotmail.com>
2026-04-29 17:43:17 -0300

    Pontos acm -> vida, poitivo e negativo (d7, d14, d28 e d56)

--- projeto/etl.sql
+++ projeto/etl.sql
@@ -28,8 +28,32 @@ SELECT
     COUNT(CASE
             WHEN DtDiff <= 14 THEN IdTransacao END) AS QtdeTransacoesD14,
     COUNT(CASE
-            WHEN DtDiff <= 7 THEN IdTransacao END) AS QtdeTransacoesD07,
-    MIN(DtDiff) AS diasUltimaTransacao
+            WHEN DtDiff <= 7 THEN IdTransacao END) AS QtdeTransacoesD7,
+    SUM(QtdePontos) AS saldoPontos,
+
+    MIN(DtDiff) AS diasUltimaTransacao,
+
+    SUM(CASE
+            WHEN QtdePontos > 0 THEN QtdePontos ELSE 0 END) AS QtdePontosPositivoVida,
+    SUM(CASE
+            WHEN QtdePontos > 0 AND DtDiff <= 56 THEN QtdePontos ELSE 0 END) AS QtdePontosPositivoD56,
+    SUM(CASE
+            WHEN QtdePontos > 0 AND DtDiff <= 28 THEN QtdePontos ELSE 0 END) AS QtdePontosPositivoD28,
+    SUM(CASE
+            WHEN QtdePontos > 0 AND DtDiff <= 14 THEN QtdePontos ELSE 0 END) AS QtdePontosPositivoD14,
+    SUM(CASE
+            WHEN QtdePontos > 0 AND DtDiff <= 7 THEN QtdePontos ELSE 0 END) AS QtdePontosPositivoD7,
+
+    SUM(CASE
+            WHEN QtdePontos < 0 THEN QtdePontos ELSE 0 END) AS QtdePontosNegativo,
+    SUM(CASE
+            WHEN QtdePontos < 0 AND DtDiff <= 56 THEN QtdePontos ELSE 0 END) AS QtdePontosNegativoD56,
+    SUM(CASE
+            WHEN QtdePontos < 0 AND DtDiff <= 28 THEN QtdePontos ELSE 0 END) AS QtdePontosNegativoD28,
+    SUM(CASE
+            WHEN QtdePontos < 0 AND DtDiff <= 14 THEN QtdePontos ELSE 0 END) AS QtdePontosNegativoD14,
+    SUM(CASE
+            WHEN QtdePontos < 0 AND DtDiff <= 7 THEN QtdePontos ELSE 0 END) AS QtdePontosNegativoD7
 FROM tb_transacoes
 GROUP BY 1
 )
